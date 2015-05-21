201fbbc0f2c7430de4c81aff3d3596bd7beeb1e9
Nathan Jones <nathan.jones2@airnz.co.nz>
2015-05-21 14:39:11 +1200

    Put logging in for the cache and source path for downloading experimental RPMs

--- recipes/install_experimental_server.rb
+++ recipes/install_experimental_server.rb
@@ -1,11 +1,15 @@
 gocd_rpm_cache_path = "#{Chef::Config[:file_cache_path]}/gocd-server-#{node[:gocd][:version]}.rpm"
+source_path = "#{node[:gocd][:download_path]}/#{node[:gocd][:version]}/go-server-#{node[:gocd][:version]}.noarch.rpm"
+Chef::Log.debug("Downloading to '#{gocd_rpm_cache_path}'")
+Chef::Log.debug("Downloading from '#{source_path}'")
+
 remote_file gocd_rpm_cache_path do
-    source "#{node[:gocd][:download_path]}/#{node[:gocd][:version]}/go-agent-#{node[:gocd][:version]}.noarch.rpm"
+    source source_path
     action :create
     not_if { ::File.exists?(gocd_rpm_cache_path) }
 end
 
-rpm_package "gocd" do
+rpm_package "gocd-server" do
     source gocd_rpm_cache_path
     action :install
 end
