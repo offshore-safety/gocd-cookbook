c4fec3a14164ae0bfb85e5628450fba1890768bc
Nathan Jones <nathan.jones2@airnz.co.nz>
2015-05-21 15:14:05 +1200

    Bump the logging output for the source and cache path to INFO

--- recipes/install_experimental_agent.rb
+++ recipes/install_experimental_agent.rb
@@ -1,7 +1,7 @@
 gocd_rpm_cache_path = "#{Chef::Config[:file_cache_path]}/gocd-agent-#{node[:gocd][:version]}.rpm"
 source_path = "#{node[:gocd][:download_path]}/#{node[:gocd][:version]}/go-agent-#{node[:gocd][:version]}.noarch.rpm"
-Chef::Log.debug("Downloading to '#{gocd_rpm_cache_path}'")
-Chef::Log.debug("Downloading from '#{source_path}'")
+Chef::Log.info("Downloading to '#{gocd_rpm_cache_path}'")
+Chef::Log.info("Downloading from '#{source_path}'")
 
 remote_file gocd_rpm_cache_path do
     source source_path
